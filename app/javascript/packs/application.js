a5879e18000140aebdd12f76787aa973f2505cbc
Justyna Wojtczak <justine84@gmail.com>
2021-09-04 23:24:27 +0000

    Setup Leaflet lib + gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,15 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
+import * as L from 'leaflet';
+import 'leaflet-defaulticon-compatibility';
---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,4 @@
+Leaflet.tile_layer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+Leaflet.attribution = "Your attribution statement"
+Leaflet.max_zoom = 18
